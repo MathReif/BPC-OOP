b87f5b576a37928f4fc82b0b0e05fd592ff19d2d
MathReif <videopozeram@gmail.com>
2025-02-26 17:35:13 +0100

    nw

--- cv03/Program.cs
+++ cv03/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var myArray = new double[2, 3]
+       {
+            {1.3, 1.1, 7.5},
+            {8, 5.4, 6.2}
+       };
+var myArray2 = new double[2, 3]
+       {
+            {1.0, 2.0, 3.0},
+            {4.0, 5.0, 6.0}
+       };
+
+var myArray3 = new double[2, 2]
+       {
+            {1.2, 2.1},
+            {4.4, 5.1}
+       };
+var myArray4 = new double[3, 2]
+       {
+            {7, 8},
+            {9, 10},
+            {11, 12}
+       };
+Matrix matica = new Matrix(myArray);
+Matrix matica2 = new Matrix(myArray2);
+
+Console.WriteLine(Matica1 + Matica2);
--- cv03/matrix.cs
+++ cv03/matrix.cs
@@ -1,12 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace cv03
+
+internal class Matrix
 {
-    internal class matrix
+    private double[,] matrix;
+
+    public Matrix(double[,] matrix)
     {
+        this.matrix = matrix;
     }
+    private int RowSize
+    {
+        get
+        {
+            return matrix.GetLength(0);
+        }
+    }
+    private int ColumnSize
+    {
+        get
+        {
+            return matrix.GetLength(1);
+        }
+    }
+    public static Matrix operator +(Matrix a, Matrix b)
+    {
+        if (a.RowSize != b.RowSize || a.ColumnSize != b.ColumnSize)
+        {
+            throw new Exception("Incopatible matrix size");
+        }
+        double[,] resultMatrix = new double[a.RowSize, a.ColumnSize];
+
+        for (int rowIndex = 0; rowIndex < a.RowSize; rowIndex++)
+        {
+            for (int columnIndex = 0; columnIndex < a.ColumnSize; columnIndex++)
+            {
+                resultMatrix[rowIndex, columnIndex] = a.matrix[rowIndex, columnIndex] + b.matrix[rowIndex, columnIndex];
+            }
+        }
+        return new Matrix(resultMatrix);
+    }
+
+    public static Matrix operator -(Matrix a, Matrix b)
+    {
+        if (a.RowSize != b.RowSize || a.ColumnSize != b.ColumnSize)
+        {
+            throw new Exception("Incopatible matrix size");
+        }
+        double[,] resultMatrix = new double[a.RowSize, a.ColumnSize];
+
+        for (int rowIndex = 0; rowIndex < a.RowSize; rowIndex++)
+        {
+            for (int columnIndex = 0; columnIndex < a.ColumnSize; columnIndex++)
+            {
+                resultMatrix[rowIndex, columnIndex] = a.matrix[rowIndex, columnIndex] - b.matrix[rowIndex, columnIndex];
+            }
+        }
+        return new Matrix(resultMatrix);
+    }
+
+    public override string ToString()
+    {
+        int row_num = matrix.GetLength(0);
+        int col_num = matrix.GetLength(1);
+        string returnString = "";
+        for (int i = 0; i < row_num; i++)
+        {
+            for (int j = 0; j < col_num; j++)
+            {
+                returnString = returnString + matrix[i, j].ToString() + " ";
+
+            }
+            returnString = returnString + "\n";
+        }
+        return returnString;
+    }
+
+
 }
