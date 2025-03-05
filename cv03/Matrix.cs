17b4416de3dbc897937bc2bd98169b51d77143a3
MathReif <videopozeram@gmail.com>
2025-03-05 17:30:11 +0100

    xd

---
+++ cv03/Matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    internal class Matrix
+    {
+        public double[,] matrix;
+        
+        public Matrix(double[,] matrix)
+        {
+            this.matrix = matrix;
+
+        }
+    }
--- cv03/Program.cs
+++ cv03/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] myArray = 
+       {
+            {1.3, 1.1, 7.5},
+            {8, 5.4, 6.2}
+       };
+var myArray2 = new double[2, 3]
+       {
+            {1.0, 2.0, 3.0},
+            {4.0, 5.0, 6.0}
+       };
+
+var myArray3 = new double[2, 2]
+       {
+            {1.2, 2.1},
+            {4.4, 5.1}
+       };
+var myArray4 = new double[3, 2]
+       {
+            {7, 8},
+            {9, 10},
+            {11, 12}
+       };
+double[,] xd = { { 1.2 }, { 1.3 } };
+
+Matrix xd1 = new Matrix(xd);
+
+Console.WriteLine(xd1.matrix);
