322630ed02646c2dc21e77e5f693078d80dffef7
MathReif <videopozeram@gmail.com>
2025-03-24 13:25:20 +0100

    added things

---
+++ cv07/Kruh.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv07
+{
+    public class Kruh :Objekt2D
+    {
+        public double radius { get; set; }
+        
+        public Kruh(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double Plocha()
+        {
+            return 2*radius*Math.PI;
+        }
+
+        public override string ToString()
+        {
+            return $"{Plocha()}";
+        }
+    }
+}
---
+++ cv07/Objekt2D.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv07
+{
+    public abstract class Objekt2D : I2D
+    {
+        public abstract double Plocha();
+        public interface IComparable
+        {
+            double CompareTo(Objekt2D obj);
+        }
+    }
+}
