70fab4858dc9e71de7bc9e32bb57fe22e470f786
MathReif <videopozeram@gmail.com>
2025-04-07 01:48:45 +0200

    hotovo snad

---
+++ cv09/Calculator.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv09
+{
+    public class Calculator
+    {
+        private enum Stav
+        {
+            PrvniCislo,
+            Operace,
+            DruheCislo,
+            Vysledek
+        };
+
+        private Stav _stav;
+
+        public string Display { get; private set; }
+        public string Pamet { get; private set; }
+
+        private double _prvniCislo;
+        private double _druheCislo;
+        private string _operace;
+        private string _buffer; // načítání čísla
+
+        public Calculator()
+        {
+            _stav = Stav.PrvniCislo;
+            Display = "0";
+            Pamet = "";
+            _buffer = "";
+        }
+
+        public void Tlacitko(string vstup)
+        {
+            if (double.TryParse(vstup, out double cislo))
+            {
+                // Znak je číslo nebo desetinná čárka
+                _buffer += vstup;
+                Display = _buffer;
+            }
+            else
+            {
+                switch (vstup)
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                        if (_stav == Stav.PrvniCislo && _buffer != "")
+                        {
+                            _prvniCislo = double.Parse(_buffer);
+                            _buffer = "";
+                            _operace = vstup;  // Použi celý string
+                            _stav = Stav.Operace;
+                        }
+                        else if (_stav == Stav.Vysledek)
+                        {
+                            _prvniCislo = double.Parse(Display);
+                            _operace = vstup;  // Použi celý string
+                            _stav = Stav.Operace;
+                        }
+                        break;
+
+                    case "=":
+                        if (_stav == Stav.Operace)
+                        {
+                            _stav = Stav.DruheCislo;
+                            _druheCislo = double.Parse(_buffer);
+                            _buffer = "";
+                            Vypocitej();
+                            _stav = Stav.Vysledek;
+                        }
+                        else if (_stav == Stav.DruheCislo && _buffer != "")
+                        {
+                            _druheCislo = double.Parse(_buffer);
+                            _buffer = "";
+                            Vypocitej();
+                            _stav = Stav.Vysledek;
+                        }
+                        break;
+
+                    case "C":
+                        // Clear
+                        _stav = Stav.PrvniCislo;
+                        _buffer = "";
+                        _prvniCislo = 0;
+                        _druheCislo = 0;
+                        _operace = " ";
+                        Display = "0";
+                        Pamet = "";
+                        break;
+                        case "+/-":
+    if (_stav == Stav.PrvniCislo || _stav == Stav.Operace)
+                        {
+                            // Ak si v stave Operace a buffer je prázdny, dovoľ užívateľovi zadať záporné číslo
+                            if (_buffer == "")
+                            {
+                                _buffer = "-";
+                            }
+                            else if (_buffer[0] == '-')
+                            {
+                                _buffer = _buffer.Substring(1);
+                            }
+                            else
+                            {
+                                _buffer = "-" + _buffer;
+                            }
+                            Display = _buffer;
+                        }
+                        else if (_stav == Stav.Vysledek)
+                        {
+                            // Ak je zobrazený výsledok, tak zmeň znamienko výsledku
+                            double vysledek = double.Parse(Display);
+                            vysledek *= -1;
+                            Display = vysledek.ToString();
+                            _buffer = Display;
+                            _stav = Stav.PrvniCislo;
+                        }
+                        break;
+                    case ",":
+                        if (_stav == Stav.Vysledek)
+                        {
+                            _buffer = Display;  // Prevezmi výsledok do bufferu
+                            _stav = Stav.PrvniCislo;
+                        }
+
+                        if (!_buffer.Contains(","))
+                        {
+                            if (_buffer == "")
+                            {
+                                _buffer = "0,";
+                            }
+                            else
+                            {
+                                _buffer += ",";
+                            }
+                            Display = _buffer;
+                        }
+                        break;
+                    case "CE":
+                        if (_stav == Stav.Vysledek)
+                        {
+                            // Po výpočte resetuj celý kalkulátor
+                            _buffer = "";
+                            _prvniCislo = 0;
+                            _operace = "";
+                            _stav = Stav.PrvniCislo;
+                        }
+                        else
+                        {
+                            // Inak len vymaž aktuálny vstup
+                            _buffer = "";
+                        }
+                        Display = "0";
+                        break;
+                }
+            }
+        }
+
+        private void Vypocitej()
+        {
+            double vysledek = 0;
+            switch (_operace)
+            {
+                case "+": vysledek = _prvniCislo + _druheCislo; break;
+                case "-": vysledek = _prvniCislo - _druheCislo; break;
+                case "*": vysledek = _prvniCislo * _druheCislo; break;
+                case "/": vysledek = (_druheCislo != 0) ? _prvniCislo / _druheCislo : 0; break;
+            }
+
+            Display = vysledek.ToString();
+            Pamet = "M";
+        }
+    }
+}
---
+++ cv09/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace cv09;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private Calculator calculator = new Calculator();
+
+    private bool isDarkMode = true;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Loaded += MainWindow_Loaded; // Apply button styles after full load
+        display.Content = calculator.Display;
+        pamet.Content = calculator.Pamet;
+
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        var initialStyle = this.FindResource("DarkButton") as Style;
+        ApplyStyleToAllButtons(initialStyle);
+        ThemeToggleButton.Style = this.FindResource("DarkButton") as Style;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        
+        calculator.Tlacitko((sender as Button).Content.ToString());
+        display.Content = calculator.Display;
+        pamet.Content = calculator.Pamet;
+        
+    }
+
+    private void ThemeToggleButton_Click(object sender, RoutedEventArgs e)
+    {
+        isDarkMode = !isDarkMode;
+
+        // Colors
+        string bgColor = isDarkMode ? "#1e1e1e" : "White";
+        string fgColor = isDarkMode ? "White" : "Black";
+        string labelBg = isDarkMode ? "#1e1e1e" : "White";
+        string memFg = isDarkMode ? "LightGray" : "Gray";
+
+        this.Background = (Brush)new BrushConverter().ConvertFromString(bgColor);
+        display.Foreground = (Brush)new BrushConverter().ConvertFromString(fgColor);
+        display.Background = (Brush)new BrushConverter().ConvertFromString(labelBg);
+        pamet.Foreground = (Brush)new BrushConverter().ConvertFromString(memFg);
+        pamet.Background = (Brush)new BrushConverter().ConvertFromString(labelBg);
+
+        ThemeToggleButton.Content = isDarkMode ? "🌙" : "☀️";
+
+        // Button style switch
+        var newStyle = this.FindResource(isDarkMode ? "DarkButton" : "LightButton") as Style;
+        ApplyStyleToAllButtons(newStyle);
+        ThemeToggleButton.Style = newStyle; // ✅ This line makes the toggle match
+    }
+
+    // Helper to walk visual tree and apply style
+    private void ApplyStyleToAllButtons(Style newStyle)
+    {
+        void RecurseChildren(DependencyObject parent)
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child is Button btn && btn != ThemeToggleButton)
+                {
+                    btn.Style = newStyle;
+                }
+                else
+                {
+                    RecurseChildren(child); // keep searching
+                }
+            }
+        }
+
+        RecurseChildren(this); // start from main window
+    }
+}
